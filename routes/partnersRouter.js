ab8d86bef057167a6a58d7939467e9b4da99e369
quantum.v.rl <quantum.v.rl@gmail.com>
2020-03-16 22:30:08 -0700

    Express Generator REST API

---
+++ routes/partnersRouter.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const partnersRouter = express.Router();
+
+
+
+partnersRouter.use(bodyParser.json());
+// Chain route method
+partnersRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send all the partners to you`);
+})
+.post((req, res) => {
+    res.end(`Will add the partners: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partners');
+})
+.delete((req, res) => {
+    res.end('Deleting all partners');
+});
+
+//Workshop add-on
+partnersRouter.route('/:partnersId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get('/:partnersId', (req, res) => {
+    res.end(`Will send details of the partners: ${req.params.partnersId} to you`);
+})
+
+.post('/:partnersId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /partners/${req.params.partnersId}`);
+})
+
+.put('/:partnersId', (req, res) => {
+    res.write(`Updating the partners: ${req.params.partnersId}\n`);
+    res.end(`Will update the partners: ${req.body.name}
+        with description: ${req.body.description}`);
+})
+
+.delete('/:partnersId', (req, res) => {
+    res.end(`Deleting partners: ${req.params.partnersId}`);
+});
+
+module.exports = partnersRouter;
