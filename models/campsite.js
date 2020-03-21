c7eeb20ccf3ec344242f15001f7adfc0cfbe61a6
quantum.v.rl <quantum.v.rl@gmail.com>
2020-03-21 10:50:53 -0700

    fix err

--- models/campsite.js
+++ models/campsite.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-require('mongoose-currency').loadType(mongoose);
-const Currency = mongoose.Types.Currency;
-
-const campsiteSchema = new Schema({
+const commentSchema = new Schema({
     rating: {
         type: Number,
         min: 1,
@@ -18,28 +15,11 @@ const campsiteSchema = new Schema({
     author: {
         type: String,
         required: true
-    },
-    image: {
-        type: String,
-        required: true
-    },
-    elevation: {
-        type: Number,
-        required: true
-    },
-    cost: {
-        type: Currency,
-        required: true,
-        min: 0
-    },
-    featured: {
-        type: Boolean,
-        default: false
-    },
-    comments: [commentSchema]
+    }
 }, {
     timestamps: true
 });
+    
 
 const campsiteSchema = new Schema({
     name: {
@@ -56,6 +36,6 @@ const campsiteSchema = new Schema({
     timestamps: true
 });
 
-const Campsite = mongoose.model('Campsite', campsiteSchema);
 
+const Campsite = mongoose.model('Campsite', campsiteSchema);
 module.exports = Campsite;
