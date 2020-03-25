309ac4e9262e8341182cbefa4971a5fea07e0250
quantum.v.rl <quantum.v.rl@gmail.com>
2020-03-24 23:13:51 -0700

    Express Session Part 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,14 +17,14 @@ const mongoose = require('mongoose');
 
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-connect.then(() => console.log('Connected correctly to server'), 
-    err => console.log(err)
+connect.then(() => console.log('Connected correctly to server'),
+  err => console.log(err)
 );
 
 var app = express();
@@ -33,31 +35,77 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
+//app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-//Implement a REST API Exercise: Express Generator//
-app.use('/campsites', campsiteRouter);
-app.use('/promotions', promotionRouter);
-app.use('/partners', partnersRouter);
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+//Basic Authentication
+function auth(req, res, next) {
+  console.log(req.session);
+  
+  if (!req.session.user) {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+          const err = new Error('You are not authenticated!');
+          res.setHeader('WWW-Authenticate', 'Basic');
+          err.status = 401;
+          return next(err);
+      }
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+      const user = auth[0];
+      const pass = auth[1];
+      if (user === 'admin' && pass === 'password') {
+          req.session.user = 'admin';
+          return next(); // authorized
+      } else {
+          const err = new Error('You are not authenticated!');
+          res.setHeader('WWW-Authenticate', 'Basic');
+          err.status = 401;
+          return next(err);
+      }
+  } else {
+      if (req.session.user === 'admin') {
+        console.log('req.session:', req.session);
+          return next();
+      } else {
+          const err = new Error('You are not authenticated!');
+          err.status = 401;
+          return next(err);
+      }
+  }
+}
+    app.use(auth);
+    //above function enoperable
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    app.use('/', indexRouter);
+    app.use('/users', usersRouter);
+    //Implement a REST API Exercise: Express Generator//
+    app.use('/campsites', campsiteRouter);
+    app.use('/promotions', promotionRouter);
+    app.use('/partners', partnersRouter);
 
-module.exports = app;
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+      next(createError(404));
+    });
+
+    // error handler
+    app.use(function (err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
+
+    module.exports = app;
