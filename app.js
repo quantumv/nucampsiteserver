dbfc62d164b6372581a0b1f194e922a723360362
quantum.v.rl <quantum.v.rl@gmail.com>
2020-03-24 23:58:13 -0700

    Express Session Part 2

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 //Implement a REST API Exercise: Express Generator//
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
@@ -46,48 +47,32 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
 //Basic Authentication
 function auth(req, res, next) {
   console.log(req.session);
   
   if (!req.session.user) {
-      const authHeader = req.headers.authorization;
-      if (!authHeader) {
-          const err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');
-          err.status = 401;
-          return next(err);
-      }
-
-      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-      const user = auth[0];
-      const pass = auth[1];
-      if (user === 'admin' && pass === 'password') {
-          req.session.user = 'admin';
-          return next(); // authorized
-      } else {
-          const err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');
-          err.status = 401;
-          return next(err);
-      }
-  } else {
-      if (req.session.user === 'admin') {
-        console.log('req.session:', req.session);
-          return next();
-      } else {
-          const err = new Error('You are not authenticated!');
-          err.status = 401;
-          return next(err);
-      }
-  }
+    const err = new Error('You are not authenticated!');
+    err.status = 401;
+    return next(err);
+} else {
+    if (req.session.user === 'authenticated') {
+        return next();
+    } else {
+        const err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
+    }
+}
 }
     app.use(auth);
     //above function enoperable
 
-    app.use('/', indexRouter);
-    app.use('/users', usersRouter);
-    //Implement a REST API Exercise: Express Generator//
+  //Implement a REST API Exercise: Express Generator//
     app.use('/campsites', campsiteRouter);
     app.use('/promotions', promotionRouter);
     app.use('/partners', partnersRouter);
